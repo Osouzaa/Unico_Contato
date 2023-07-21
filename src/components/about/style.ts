ce27b39e2566ed380e7248dc82d679923717b007
Osouzaa <gabrielsouzaalves750@gmail.com>
2023-07-21 20:43:03 -0300

    att

--- src/components/about/style.ts
+++ src/components/about/style.ts
@@ -2,11 +2,14 @@ import styled from "styled-components";
 
 const Container = styled.div`
   background-color: #003;
+  display:flex;
+  justify-content:center;
+  align-items:center;
   padding: 2em 0 0 0;
   height: 700px;
 
   @media (max-width: 414px) {
-      height: 300px; 
+      height: 350px; 
       padding: 2em 0 0 0;
     }
 
@@ -17,8 +20,8 @@ const Container = styled.div`
     width: 100%;
 
     @media (max-width: 414px) {
-      height: 350px; 
-      width: 115%;
+      height: 100%; 
+      width: 100%;
     }
   }
 `;
